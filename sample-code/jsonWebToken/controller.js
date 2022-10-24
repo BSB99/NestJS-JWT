cc79061a2852db71ef9ba94c8d4d7845a48aaa1f
BaekSeungBum <wer06099@naver.com>
2022-10-24 09:56:16 +0900

    Add(Seungbum): base64url

--- sample-code/jsonWebToken/controller.js
+++ sample-code/jsonWebToken/controller.js
@@ -10,12 +10,16 @@ router.post('/login', (req, res, next) => {
   //header 의 기본값은  alg: 'HS256', typ: 'JWT', 이다.
 
   //jwt.sign(payload, secretOrPrivateKey, [options, callback])
+  // encoding 기본 값 -> base64url
   token = jwt.sign(
     {
       email: email,
       name: name,
     },
     SECRET_KEY,
+    // {
+    // encoding: 'base64-url',
+    // },
     // {
     //   expiresIn: '15m', // 만료시간 15분
     //   issuer: '토큰발급자',
@@ -26,7 +30,7 @@ router.post('/login', (req, res, next) => {
   return res.status(200).json({
     code: 200,
     message: '토큰이 발급되었습니다.',
-    token: token,
+    token,
   });
 });
 
